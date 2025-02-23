26aa6b1da0b064d18bd7dea1d3bfe5ff16d14b44
juanledesmaa <umbranito@gmail.com>
2025-02-22 23:11:32 -0400

    Add Model Layer

---
+++ My-Recipes-Private/Domain/Entities/Recipe.swift
@@ -0,0 +1,19 @@
+//
+//  Recipe.swift
+//  My-Recipes-Private
+//
+//  Created by Juanito on 2/19/25.
+//
+
+import Foundation
+
+struct Recipe: Codable {
+	let uuid: String
+	let name: String
+	let cuisine: String
+	
+	let photoUrlLarge: String?
+	let photoUrlSmall: String?
+	let sourceUrl: String?
+	let youtubeUrl: String?
+}
---
+++ My-Recipes-Private/Domain/Entities/RecipesList.swift
@@ -0,0 +1,12 @@
+//
+//  RecipesList.swift
+//  My-Recipes-Private
+//
+//  Created by Juanito on 2/22/25.
+//
+
+import Foundation
+
+struct RecipesList: Codable {
+	let recipes: [Recipe]
+}
